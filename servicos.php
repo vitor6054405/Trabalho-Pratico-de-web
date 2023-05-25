b94d9435f3e2d66c516243ccf16a79437c32d38d
Vitor Gabriel <114226983+vitor6054405@users.noreply.github.com>
2023-05-24 21:32:40 -0300

    Update servicos.php

---
+++ servicos.php
@@ -0,0 +1,235 @@
+<?php
+    $procedimentos = [
+    [
+        "nome" => "Unhas",
+        "foto" =>
+        "imagens/unhas.png",
+        "descricao" => "Ao entender melhor como funcionam os aplicativos de manicure delivery,
+        você já deve ter percebido todas as vantagens que eles oferecem para o seu dia a dia. São
+        diversos benefícios, afinal, você pode contratar os serviços em qualquer dia e para qualquer
+        situação. Segue os valores dos serviços:" ,
+        "serviços" =>"<ul><li>Unha da mão: 60 Reais</li>
+        <li> Unha do pé: 75 Reais</li>
+        <li>Pé e mão: 130 Reais</li>
+        <li>Apenas cuticulagem pé e mão: 40 Reais</li>
+        <li>Fibra: 200 Reais</li>
+        <li>Gel: 100 Reais </li>",
+    ],
+
+    [
+        "nome" => "Massagem",
+        "foto" => "imagens/massagem.png",
+        "descricao" => "Sua rotina é cansativa, você não tem um momento para si mesmo, está
+        constantemente sob estresse e mal consegue dormir à noite pensando em tudo o que tem
+        para fazer? Você encontraria um espaço nas suas tarefas diárias para se dedicar a um instante
+        de relaxamento e aproveitar para ficar zen? Então, nós temos a solução perfeita! Seu momento
+        relax pode acontecer em qualquer lugar e a qualquer instante, desde que você contate um dos
+        nossos profissionais e esteja disponível. Afinal, nada melhor do que receber uma boa
+        massagem, tirar os pontos doloridos do corpo, desatar alguns nós e poder, simultaneamente,
+        trabalhar a sua respiração, esvaziar a mente de uma série de problemas e, ao menos por algum
+        período, focar apenas em você, no seu relaxamento e nessa troca de energias." ,
+        "serviços" =>"<ul><li>Geral: 260 reais</li>
+        <li>Costas: 120 reais</li>
+        <li>Pés e mãos: 100 reais</li>
+        <li>Básica: 180 reais</li>
+        <li>Relaxante: 210 reais</li>
+        <li>Especial: 290 reais</li></ul>",
+    ],
+
+    [
+        "nome" => "Maquiagem",
+        "foto" => "imagens/make.png",
+        "descricao" => "Você tem um casamento para ir ou uma formatura – seja como formanda
+        ou convidada? Seja qual for o seu evento, é primordial estar com a aparência em dia e garantir
+        que você se sinta bem consigo mesmo. Afinal, seu rosto é uma das principais partes que levam
+        os outros indivíduos a construírem uma imagem a seu respeito e, por esse motivo, ele precisa
+        estar apresentável e de acordo com o que você deseja, não é mesmo?
+        Em muitos casos, uma face limpa e sem maquiagem é uma verdadeira tela em branco e,
+        claro, é nela que os melhores profissionais vão realizar uma completa obra de arte, dando as
+        nuances desejadas, criando sombra nos locais que precisam ser ocultados, iluminando as
+        regiões que mais se destacam em seu rosto e, acima de tudo, ajudando a cliente a ganhar
+        aquela sensação de estar em paz com o espelho.
+        " ,
+        "serviços" =>"<ul><li>Pele: 80 reais</li>
+        <li>Completa: 130 reais</li>
+        <li>Básica: 100 reais</li>
+        <li>Especial: 150 reais</li></ul>",
+    ],
+
+    [
+        "nome" => "Estética",
+        "foto" => "imagens/estetica.png",
+        "descricao" => "",
+        "serviços" =>"<ul><li>skin care básica: 120 reais</li>
+        <li>Completa: 160 reais</li>
+        <li>Limpeza de pele: 100 reais</li>
+        <li>Tratamento de manchas: 130 reais</li></ul>
+        ",
+    ],
+
+    [
+        "nome" => "Depilação",
+        "foto" => "imagens/depilacao.png",
+        "descricao" => "A depilação faz parte da rotina de cuidados estéticos de mulheres e
+        homens, e proporciona uma sensação de leveza na pele e bem-estar. Basicamente, depilação é
+        o ato de retirar propositalmente os pelos do corpo, seja de maneira temporária ou definitiva.
+        Métodos de depilação profissional
+        Nas últimas décadas, a depilação evoluiu e hoje os métodos se tornaram mais seguros e
+        duradouros.
+
+        Cera fria
+        A cera é espalhada em folha de celofane e aplicada sobre a região desejada. O processo
+        deve se repetir até que a folha perca a capacidade de retirar os pelos. Seu resultado pode ser
+        aproveitado por 20 ou 30 dias.
+
+        É um método financeiramente acessível e higiênico, devido ao uso de material descartável
+        e da facilidade de usar em casa. Além disso, é ideal para pessoas que possuem vasos dilatados
+        ou sensibilidade a altas temperaturas.
+
+        Cera quente
+        É uma das técnicas mais populares. A cera aquecida por um aparelho é aplicada ainda
+        quente sobre a pele e removida, geralmente, com o auxílio de uma tira de pano ou papel. A
+        temperatura do produto dilata os poros e torna a retirada do pelo mais fácil. Com o tempo, o
+        volume de pelos na região frequentemente submetida à cera quente tende a diminuir. O
+        resultado do processo dura entre 20 e 30 dias.
+
+        Linha
+        É um procedimento oriental que consiste na retirada dos pelos por meio de uma linha. É
+        indicado para fios mais finos, como os da região das sobrancelhas e buço. Dura em torno de 30
+        dias.
+
+        Entre suas vantagens está o fato de não envolver substâncias químicas. Desse modo,
+        diminui a incidência de irritação ou alergia na pele.
+
+        Fotodepilação
+        Esse é um método de depilação que é capaz de eliminar 95% dos pelos definitivamente.
+        Utiliza a tecnologia chamada de Luz Pulsada Intensa que acaba com o folículo aos poucos.
+
+        Essa luz é suave, causando menos dor e menos agressão à pele. Outro benefício é que
+        pode ser usada em qualquer área do corpo e funciona independente do tom de pele.
+
+        Laser
+        O laser age identificando pontos de melanina e destruindo a estrutura responsável por
+        criar novos pelos. É feito por sessões e o resultado esperado costuma ser entregue após 8 ou
+        10 aplicações.
+
+        Funciona apenas em fios escuros em pele mais clara. Remove cerca de 80% dos fios em
+        definitivo, mas sessões anuais de manutenção são recomendadas.
+
+        " ,
+        "serviços" =>"<ul><li>Completa: 500 reais</li>
+        <li> Virilhas: 280 reais</li>
+        <li>Axilas: 100 reais</li>
+        <li>Pernas: 200 reais</li>
+        <li>Braços: 210 reais</li>
+        <li>Sobrancelha: 30 reais</li>
+        <li>Buço: 25 reais</li></ul>
+        ",
+
+    ],
+
+    [
+        "nome" => "Cabelos",
+        "foto" => "imagens/cabelo.png",
+        "descricao" => "A comodidade que você procura, com a qualidade que precisa! Dê às suas
+        madeixas a atenção que elas precisam! Nossos profissionais se adequam ao estilo de cabelo
+        que você procura, proporcionando muito zelo e dedicação, a fim de garantir a entrega de um
+        trabalho qualitativo que atinja as expectativas aguardadas.
+        Hidratação
+        Um dos mais populares tratamentos capilares, responsável pela reposição de água e
+        nutrientes importantes que são evacuados diariamente, a hidratação garante a qualidade e
+        saúde do cabelo contra o ressecamento dos fios.
+        Nutrição
+        Com a função de ser uma camada que protege os fios de cabelo, a nutrição capilar é o
+        melhor caminho para que as madeixas permaneçam preservadas e saudáveis.
+        Reconstrução
+        A mais potente etapa do cronograma capilar. É responsável pela reconstrução de acordo
+        com a verdadeira necessidade do cabelo. Age diretamente nos fios danificados que necessitam
+        de uma atenção maior, como a recuperação da massa capilar.
+        Beleza e bem-estar com um clique
+        Além de importantes dicas para a saúde capilar, os melhores profissionais estão aqui. Viva
+        uma experiência única e consulte os nossos serviços.
+        Corte
+        Cada cliente exige uma comunicação que se adeque ao seu estilo de vida. O papel do
+        profissional cadastrado no Belezap não é apenas executar, mas também ouvir o cliente, para
+        poder compreender as suas necessidades. O corte de cabelo ideal necessita dessa atenção
+        especial para um resultado final plenamente satisfatório.
+        Coloração
+        A escolha dos profissionais certos pode auxiliar na escolha de um tom perfeito, com todas
+        as características adequadas para a nova adaptação.
+        Escova
+        Produtos termoativados que garantem a qualidade no tratamento da secagem e
+        remodelagem dos fios, protegendo-os de efeitos ocasionados pelo ar quente.
+        Lavagem
+        A temperatura da água, o tipo de shampoo adequado a cada tipo de cabelo, o tempo e os
+        movimentos adequados para massagear os cabelos da maneira correta, também são sugeridos
+        e praticados pelos nossos profissionais.
+        Relaxamento e alisamento
+        O relaxamento dos fios retira aquele volume excessivo dos cachos, o que pode ser uma
+        boa solução para as pessoas de cabelos cacheados que não gostam dos seus cachinhos tão
+        volumosos, enquanto o alisamento dos fios deixa o cabelo “chapado”, devido a um processo
+        químico mais forte. Nossos profissionais podem indicar o que se adequa melhor a cada cabelo
+        e estilo.
+        Através do aplicativo Belezap, você tem acesso a todo o nosso conteúdo e serviços como
+        os citados acima, além de muitos outros. Tire suas dúvidas com nossos profissionais e agende
+        seu horário. Você pode obter nossos serviços no conforto da sua casa! Tudo isso, através da
+        tela do seu celular." ,
+        "serviços" =>"<ul><li>Hidratação: 220 reais</li>
+        <li>Pintura: 190 reais</li>
+        <li> Luzes: 400 reais</li>
+        <li> Moreno iluminado: 430 reais</li>
+        <li>Cortes: 170 reais</li>
+        <li>Chapinha: 90 reais</li>
+        <li>Escova: 130 reais</li>
+        <li>Finalização: 150 reais</li></ul>
+        ",
+    ],
+
+    [
+        "nome" => "Barbearia",
+        "foto" => "imagens/barbearia.png",
+        "descricao" => "Toda a nossa tradição, agora até a sua casa no serviço de barbeiro delivery.
+        Os barbeiros e a experiência da Singu que você já conhece!
+        Equipe especializada com anos de experiência, cursos, workshops, biossegurança,
+        visagismo e
+        atendimento ao cliente e muito mais.
+        Cortes na tesoura, máquina, barba curta, média ou lumberjack. Cabelos compridos ou
+        curtos.
+
+        São anos de experiência agora no conforto da sua casa.
+
+        * Profissionais uniformizados e identificados
+
+        * Utilização de máscaras de proteção
+
+        * Álcool 70º e álcool gel para esterilização dos equipamentos e mãos.
+
+        * Proteção nos pés com materiais descartáveis
+
+        * Profissionais capacitados e treinados com as mais rígidas normas de biossegurança
+        " ,
+        "serviços" =>"<ul><li>Bigode: 50 reais</li>
+        <li> Cabelo: 75 reais</li>
+        <li>Barba: 60 reais</li>
+        <li> Completo: 130 reais</li>
+        <li>Sobrancelha: 30 reais</li>
+        <li>Tintura: 150 reais</li>
+        <li>Nevou: 140 reais</li>
+        <li>Luzes: 100 reais</li></ul>
+
+        "
+    ],
+    [
+        "nome" => "Spar",
+        "foto" => "imagens/spa.png",
+        "descricao" => "Nunca é tarde demais para começar a cuidar da pele! O maior órgão do corpo humano é a primeira barreira de proteção contra as agressões externas, como raios solares e a poluição, e também é diretamente afetada por fatores internos, como a alimentação e alterações hormonais. Cada vez mais pessoas estão entrando na onda do skincare, porém, mais do que uma tendência, os cuidados com o rosto são essenciais para uma pele bonita, saudável e protegida contra o envelhecimento.
+        O SPA Posse do Corpo é um refúgio para quem busca cuidar da mente, do corpo e da alma. Com uma abordagem holística, que une terapias de relaxamento, atividades físicas, alimentação saudável e cuidados estéticos, o Posse do Corpo oferece uma experiência completa de renovação e revitalização. Aqui, você é convidado a deixar para trás as preocupações do dia a dia e se permitir conectar consigo mesmo, encontrando equilíbrio e bem-estar." ,
+        "serviços" =>"<ul><li>Massagens Relaxantes: 320 reais</li>
+        <li>Spar Day: 460 reais</li>
+        <li> Completo: 300 reais</li>
+        <li>Aromaterapia: 340 reais</li>
+        <li>Acupuntura: 330 reais</li>
+        <li>Reflexologia: 340 reais</li>
+        <li>Shiatsu: 400 reais</li></ul>"
+    ]
+    ]
